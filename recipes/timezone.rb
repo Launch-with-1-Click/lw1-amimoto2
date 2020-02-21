19510c81933b222808fff0f8f1feca7b1298703f
wokamoto <wokamoto@digitalcube.jp>
2020-02-21 16:14:58 +0900

    /etc/sysconfig/clock が無い際にエラー終了していたので修正

--- recipes/timezone.rb
+++ recipes/timezone.rb
@@ -2,6 +2,19 @@ link "/etc/localtime" do
 	to "/usr/share/zoneinfo/" + node[:timezone]
 end
 
-clock = Chef::Util::FileEdit.new("/etc/sysconfig/clock")
-clock.search_file_replace_line(/^ZONE=.*$/, "ZONE=\"#{node[:timezone]}\"")
-clock.write_file
+template "/etc/sysconfig/clock" do
+	source 'clock.erb'
+	variables(
+		:timezone => node[:timezone]
+	)
+	mode '0644'
+	not_if {File.exist?("/etc/sysconfig/clock")}
+end
+ruby_block 'clock rewrite' do
+	block do
+		clock = Chef::Util::FileEdit.new("/etc/sysconfig/clock")
+		clock.search_file_replace_line(/^ZONE=.*$/, "ZONE=\"#{node[:timezone]}\"")
+		clock.write_file
+	end
+	only_if {File.exist?("/etc/sysconfig/clock")}
+end
