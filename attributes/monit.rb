4f0fa2b6eefd9dd6e3b91f0eafb072667d51cd71
wokamoto <wokamoto@digitalcube.jp>
2021-06-04 07:14:38 +0900

    update monit conf

--- attributes/monit.rb
+++ attributes/monit.rb
@@ -8,6 +8,7 @@ default[:monit][:monitor] = {
   'php-fpm'   => node[:phpfpm][:enabled],
   'mysql'     => node[:mysql][:enabled],
   'memcached' => node[:memcached][:enabled],
+  'redis'     => node[:redis][:enabled],
   'crond'     => true,
   'logging'   => true,
 }
@@ -18,6 +19,7 @@ default[:monit][:source] = {
   'php-fpm'   => 'monit/php-fpm.erb',
   'mysql'     => 'monit/process_monitor.erb',
   'memcached' => 'monit/process_monitor.erb',
+  'redis'     => 'monit/process_monitor.erb',
   'crond'     => 'monit/process_monitor.erb',
   'logging'   => 'monit/logging.erb'
 }
@@ -26,8 +28,8 @@ default[:monit][:settings][:processes] = [
   {
    :name => 'nginx',
    :pidfile => '/var/run/nginx.pid',
-   :start => '/sbin/service nginx start',
-   :stop  => '/sbin/service nginx stop',
+   :start => '/bin/systemctl start nginx.service',
+   :stop  => '/bin/systemctl stop nginx.service',
    :user  => node[:nginx][:config][:user],
    :group => node[:nginx][:config][:group],
    :rules => [
@@ -37,8 +39,8 @@ default[:monit][:settings][:processes] = [
   {
    :name => 'httpd',
    :pidfile => '/var/run/httpd/httpd.pid',
-   :start => '/sbin/service httpd start',
-   :stop  => '/sbin/service httpd stop',
+   :start => '/bin/systemctl start httpd.service',
+   :stop  => '/bin/systemctl stop httpd.service',
    :user  => node[:nginx][:config][:user],
    :group => node[:nginx][:config][:group],
    :rules => [
@@ -47,8 +49,8 @@ default[:monit][:settings][:processes] = [
   {
    :name => 'php-fpm',
    :pidfile => '/var/run/php-fpm/php-fpm.pid',
-   :start => '/sbin/service php-fpm start',
-   :stop  => '/sbin/service php-fpm stop',
+   :start => '/bin/systemctl start php-fpm.service',
+   :stop  => '/bin/systemctl stop php-fpm.service',
    :user  => node[:php][:config][:user],
    :group => node[:php][:config][:group],
    :rules => [
@@ -57,8 +59,8 @@ default[:monit][:settings][:processes] = [
   {
    :name => 'mysql',
    :pidfile => '/var/run/mysqld/mysqld.pid',
-   :start => '/sbin/service mysqld start',
-   :stop  => '/sbin/service mysqld stop',
+   :start => '/bin/systemctl start mysqld.service',
+   :stop  => '/bin/systemctl stop mysqld.service',
    :user  => node[:mysql][:config][:user],
    :group => node[:mysql][:config][:group],
    :rules => [
@@ -66,16 +68,24 @@ default[:monit][:settings][:processes] = [
   },
   {
    :name => 'memcached',
-   :start => '/sbin/service memcached start',
-   :stop  => '/sbin/service memcached stop',
+   :start => '/bin/systemctl start memcached.service',
+   :stop  => '/bin/systemctl stop memcached.service',
+   :rules => [
+   ]
+  },
+  {
+   :name => 'redis',
+   :pidfile => '/var/run/redis/redis.pid',
+   :start => '/bin/systemctl start redis.service',
+   :stop  => '/bin/systemctl stop redis.service',
    :rules => [
    ]
   },
   {
    :name => 'crond',
    :pidfile => '/var/run/crond.pid',
-   :start => '/sbin/service crond start',
-   :stop  => '/sbin/service crond stop',
+   :start => '/bin/systemctl start crond.service',
+   :stop  => '/bin/systemctl stop crond.service',
    :rules => [
    ]
   },
