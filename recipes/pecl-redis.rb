a71fb52f228b375b11ae5ef074c662d97b0fb9a2
wokamoto <wokamoto@digitalcube.jp>
2021-08-02 15:41:17 +0900

    some fix

--- recipes/pecl-redis.rb
+++ recipes/pecl-redis.rb
@@ -2,32 +2,20 @@ bash 'pecl-igbinary' do
     action :nothing
     code <<-EOH
         pecl install igbinary
+        echo "extension=igbinary.so" > /etc/php.d/40-igbinary.ini
     EOH
-    notifies :create, "template[/etc/php.d/40-igbinary.ini]"
     notifies :reload, 'service[php-fpm]'
   
-    subscribes :run, 'yum_package[php]', :delayed
-end
-  
-template "/etc/php.d/40-igbinary.ini" do
-    action :nothing
-    source "php/php.d/40-igbinary.ini.erb"
-    notifies :reload, 'service[php-fpm]'
+    subscribes :run, 'yum_package[php-packages]', :delayed
 end
   
 bash 'pecl-redis' do
     action :nothing
     code <<-EOH
         pecl install -D 'enable-redis-igbinary="yes" enable-redis-lzf="no" enable-redis-zstd="no"' redis
+        echo "extension=redis.so" > /etc/php.d/50-redis.ini
     EOH
-    notifies :create, "template[/etc/php.d/50-redis.ini]"
     notifies :reload, 'service[php-fpm]'
   
-    subscribes :run, 'yum_package[php]', :delayed
-end
-  
-template "/etc/php.d/50-redis.ini" do
-    action :nothing
-    source "php/php.d/50-redis.ini.erb"
-    notifies :reload, 'service[php-fpm]'
+    subscribes :run, 'yum_package[php-packages]', :delayed
 end
