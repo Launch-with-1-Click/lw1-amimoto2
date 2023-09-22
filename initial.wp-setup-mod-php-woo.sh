831fee7340f306a4fd7d80162c185695af73b0a4
wokamoto <wokamoto@digitalcube.jp>
2023-09-22 15:48:44 +0900

    update php8.1

--- initial.wp-setup-mod-php-woo.sh
+++ initial.wp-setup-mod-php-woo.sh
@@ -9,7 +9,7 @@ hash jq  || /usr/bin/yum -y install jq
 
 # node[:wordpress][:woocommerce] enabled
 TMP_JSON=$(mktemp)
-/usr/bin/jq -s '.[0] * .[1]' \
+/usr/bin/jq -s '.[1] * .[0]' \
   <(echo '{"phpfpm":{"version":"81"}, "mod_php7":{"enabled":true},"wordpress":{"woocommerce":true}}') \
   /opt/local/amimoto.json \
   > ${TMP_JSON}
